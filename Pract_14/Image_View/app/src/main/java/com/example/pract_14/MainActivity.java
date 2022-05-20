b3c710c65fef532ff0609318c3deb3174fa2cff2
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-05-20 19:33:43 +0530

    Practical 15 Image view added

---
+++ Pract_14/Image_View/app/src/main/java/com/example/pract_14/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pract_14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
