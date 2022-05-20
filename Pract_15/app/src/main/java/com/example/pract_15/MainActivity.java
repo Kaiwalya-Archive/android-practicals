97606fb98ea1aa28affa08a446e0dd659e3068b7
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-05-20 20:01:23 +0530

    Practical 15 Toast added

---
+++ Pract_15/app/src/main/java/com/example/pract_15/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pract_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.btn);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello there! :D", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
